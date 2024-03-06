16a7cd370fe36cdc0ab0b0795216c511752c4793
manchyy <nmantvis@gmail.com>
2024-03-06 18:54:00 +0000

    Readme, sidebar, favicon

---
+++ README.md
@@ -0,0 +1,17 @@
+# Recogn.io
+CSSE Final Year Project
+
+## Technologies used
+Frontend - SolidJS
+Backend - Flask
+Database - MongoDB
+
+## How to run
+**Frontend**
+- CD to frontend folder
+- `npm i` 
+- `npm run dev`
+
+**Backend**
+- CD to backend folder
+- run `server.py`
--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,58 +1,115 @@
 import styles from './App.module.css';
-import { VsHome } from 'solid-icons/vs'
-import { FaSolidDiagramProject } from 'solid-icons/fa'
-import { AiOutlineTeam, AiOutlineInfoCircle } from 'solid-icons/ai'
-import { FiLogOut, FiDatabase, FiServer } from 'solid-icons/fi'
-import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@suid/material'
-import DraftsIcon from "@suid/icons-material/Drafts";
+import { 
+  Divider,
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from '@suid/material'
 import useTheme from "@suid/material/styles/useTheme";
 
+import HomeOutlinedIcon from '@suid/icons-material/HomeOutlined';
+import AccountTreeOutlinedIcon from '@suid/icons-material/AccountTreeOutlined';
+import GroupsOutlinedIcon from '@suid/icons-material/GroupsOutlined';
+import StorageOutlinedIcon from '@suid/icons-material/StorageOutlined';
+import DnsOutlinedIcon from '@suid/icons-material/DnsOutlined';
+import HelpOutlineOutlinedIcon from '@suid/icons-material/HelpOutlineOutlined';
+import LogoutOutlinedIcon from '@suid/icons-material/LogoutOutlined';
 
 function Sidebar() {
     const theme = useTheme();
     return (
       <div class={styles.sidebar}>
-        <br/>
-        <ul>
-          <li><VsHome/> Home</li>
-          <li><FaSolidDiagramProject /> Projects</li>
-          <li><AiOutlineTeam /> Team</li>
-        </ul>
-
-        <br/>
-        <hr></hr>
-        <br/>
-
-        <ul>
-          <li><FiDatabase /> Database 🟢</li>
-          <li><FiServer /> Backend 🟢</li>
-        </ul>
         
-        <br/>
-        <hr></hr>
-        <br/>
 
-        <ul>
-          <li><AiOutlineInfoCircle /> Information</li>
-          <li><FiLogOut /> Log out</li>
-        </ul>
         <List>
             <ListItem>
                 <ListItemButton>
                     <ListItemIcon>
-                        <DraftsIcon color='primary'/>
+                        <HomeOutlinedIcon 
+                          sx={{
+                            color:"white"
+                          }}
+                        />
                     </ListItemIcon>
                     <ListItemText primary="Home" />
+                </ListItemButton>
+            </ListItem>
+            <Divider light/>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                      <AccountTreeOutlinedIcon 
+                        sx={{color:'white'}}
+                      /> 
+                    </ListItemIcon>
+                    <ListItemText primary="Projects" />
                 </ListItemButton>
             </ListItem>
             <ListItem>
                 <ListItemButton>
                     <ListItemIcon>
-                        
+                      <GroupsOutlinedIcon 
+                        sx={{color:'white'}}
+                      /> 
                     </ListItemIcon>
-                    <ListItemText primary="Projects" />
+                    <ListItemText primary="Team" />
                 </ListItemButton>
             </ListItem>
+
+            {/*  */}
+            <hr></hr>
+
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                      <StorageOutlinedIcon 
+                        sx={{color:'white'}}
+                      /> 
+                    </ListItemIcon>
+                    <ListItemText primary="Database" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                      <DnsOutlinedIcon 
+                        sx={{color:'white'}}
+                      /> 
+                    </ListItemIcon>
+                    <ListItemText primary="Backend" />
+                </ListItemButton>
+            </ListItem>
+
+            {/*  */}
+            <hr></hr>
+            <Divider />
+            
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <HelpOutlineOutlinedIcon 
+                          sx={{
+                            color:"white"
+                          }}
+                        />
+                    </ListItemIcon>
+                    <ListItemText primary="Information" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem>
+                <ListItemButton>
+                    <ListItemIcon>
+                      <LogoutOutlinedIcon 
+                        sx={{color:'white'}}
+                      /> 
+                    </ListItemIcon>
+                    <ListItemText primary="Log Out" />
+                </ListItemButton>
+              </ListItem>
+
         </List>
       </div>
     );
